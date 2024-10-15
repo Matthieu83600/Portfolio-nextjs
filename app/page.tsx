ac05b5db44b57ed410a6fe117c25446a3de6cb6e
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2024-10-15 17:00:03 +0200

    fix: minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { TimeLine } from '../components/Timeline';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col">
+    <>
       <div className="bg-gradient-to-tr from-[#97f0ff] to-[#af9cfa] dark:from-[#1a1a1a] dark:to-[#3a3a3ace]">
         <div className="inset-0 bg-gradient-to-t from-slate-50 to-transparent dark:from-black">
           <IntroSection />
@@ -12,6 +12,6 @@ export default function Home() {
         </div>
       </div>
       <TimeLine />
-    </div>
+    </>
   );
 }
