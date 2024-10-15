82214018bb4a8fcb0d66861533def97a08a6b78d
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2024-10-15 15:40:43 +0200

    style: header

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,29 @@ import Footer from '../components/Footer';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Portfolio - Matthieu Bonjour',
-  description: 'Bienvenue sur mon portfolio!',
+  metadataBase: new URL('https://matthieubonjour.fr/'),
+  title: {
+    template: '%s | Portfolio MB',
+    default: 'Accueil | Portfolio MB',
+  },
+  description:
+    "Je m'appelle Matthieu Bonjour, et je suis développeur front-end junior. Bienvenue sur mon portfolio !",
+  icons: {
+    icon: '/assets/logo.svg',
+  },
+  keywords: [
+    'Matthieu Bonjour',
+    'Matthieu Bonjour - développeur',
+    'Matthieu Bonjour - developer',
+    'Développeur front-end',
+    'Frontend developer',
+    'Site portfolio',
+    'Portfolio website',
+    'Portfolio développeur front-end',
+    'Frontend Developer Portfolio',
+  ],
+  creator: 'Matthieu Bonjour',
+  authors: [{ name: 'Matthieu Bonjour', url: 'https://matthieubonjour.fr/' }],
 };
 
 export default function RootLayout({
@@ -16,7 +37,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" suppressHydrationWarning>
-      <body className="bg-slate-50 dark:bg-slate-950">
+      <body className="bg-slate-50 px-4 dark:bg-slate-950">
         <ThemeContext>
           <Header />
           <main>{children}</main>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,19 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { ThemeSwitcher } from './ThemeSwitcher';
 
 const Header = () => {
   return (
     <header className="sticky top-0 z-10 bg-inherit pb-5 pt-5 shadow-sm">
-      <div className="flex flex-col gap-4 lg:flex-row items-center justify-between p-4">
-        <h1 className='text-xl'>Mon Portfolio</h1>
+      <div className="flex flex-col items-center justify-between gap-4 lg:flex-row">
+        <Image
+          src="/assets/logo.svg"
+          width={0}
+          height={0}
+          style={{ width: '70px', height: 'auto' }}
+          loading="lazy"
+          alt="Mon logo"
+        />
         <nav>
           <ul className="flex flex-wrap items-center justify-between p-4">
             <li>
